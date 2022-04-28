ad1312d37f7eeace1af082f95ab6f15b95ff9d82
Imam_Ib <imammosa537@gmail.com>
2022-04-28 14:49:31 +0300

    update to shop.js

--- WebNNU/js/shoping.js
+++ WebNNU/js/shoping.js
@@ -1,14 +1,36 @@
-let Name =document.getElementById("clock-name").innerText;
-let salary =document.getElementById("sal").innerText;
-let img =document.getElementById("image").src;
+
+
+let countAll = document.querySelectorAll('.item').length;
+let x =0;
+let Name;
+let salary;
+let img;
+let i=1;
+let j =0;
+let s ;
+for( j=0;j<countAll;j++)
+{
+    s=document.getElementsByClassName('item')[j];
+   s.addEventListener('click',pass);
+    s.addEventListener('click', (event) => {
+    });
+
+}
+
+
+
+
+
+Name = document.getElementsByClassName("clock-name")[i].innerText;
+salary = document.getElementsByClassName("sal")[i].innerText;
+img = document.getElementsByClassName("image")[i].src;
 
 function pass()
 {
+
     localStorage.setItem('clock_Na',Name);
     localStorage.setItem('clock_Salary',salary);
     localStorage.setItem('clock_img',img);
     window.location.href = 'Product.html';
 
 }
-
-document.getElementById("item").addEventListener('click',pass);
